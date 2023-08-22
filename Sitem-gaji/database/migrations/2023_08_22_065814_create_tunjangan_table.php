9a780df3aa078983fbb63b75145c93a0dd0ca4a7
vian <galangdavianp@gmail.com>
2023-08-22 23:06:19 +0700

    CREATE TABLE

---
+++ Sitem-gaji/database/migrations/2023_08_22_065814_create_tunjangan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tunjangan', function (Blueprint $table) {
+            $table->id();
+            $table->string('potongan');
+            $table->unsignedBigInteger('id_employee');
+            $table->unsignedBigInteger('id_tunjangan');
+            $table->unsignedBigInteger('id_divisi');
+            $table->timestamps();
+
+            $table->foreign('id_employee')->references('id')->on('employee')->onDelete('cascade');
+
+            $table->foreign('id_tunjangan')->references('id')->on('tunjangan')->onDelete('cascade');
+
+            $table->foreign('id_divisi')->references('id')->on('divisi')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tunjangan');
+    }
+};
