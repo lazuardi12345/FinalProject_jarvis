97fe6bed700cb44a6798b3918fa9c3e6c5ea6839
vian <galangdavianp@gmail.com>
2023-09-24 18:50:04 +0700

    Check

--- sistem-gaji/app/Http/Middleware/CheckRole.php
+++ sistem-gaji/app/Http/Middleware/CheckRole.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckRole
@@ -18,7 +19,7 @@ class CheckRole
 
         $roles = ['admin', 'employee', 'pic'];
 
-        if (auth()->check() && in_array(auth()->user()->role, $roles)) {
+        if (auth()->check() && in_array(Auth::user()->role, $roles)) {
             return $next($request);
         }
     
